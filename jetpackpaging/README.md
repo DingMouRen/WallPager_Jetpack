02a6670d3ec61a73a64c7a5f6ca2d9850e7ed27e
cuiqingdong <naildingmouren@gmail.com>
2020-05-11 18:15:27 +0800

    workmanager

---
+++ jetpackpaging/README.md
@@ -0,0 +1,1 @@
+https://www.jianshu.com/p/abbf272b824a
--- jetpackpaging/src/main/java/com/dingmouren/jetpackpaging/MainActivity.kt
+++ jetpackpaging/src/main/java/com/dingmouren/jetpackpaging/MainActivity.kt
@@ -2,7 +2,7 @@ package com.dingmouren.jetpackpaging
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import kotlinx.android.synthetic.main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
